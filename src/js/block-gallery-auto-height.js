42a6564a9baaa996d9512f391fa668eaffaca9ee
Rich Tabor <hi@richtabor.com>
2018-11-16 22:27:25 -0600

    Delete block-gallery-auto-height.js

--- src/js/block-gallery-auto-height.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function() {
-	var elements = document.getElementsByClassName('.has-autoheight-carousel');
-	for (var i = 0; i < elements.length; i++) {
-		var flkty = Flickity.data(elements[i]);
-		flkty.resize();
-	}
-});
