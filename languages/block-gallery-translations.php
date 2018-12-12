1538718c01b25ef8d968f199f610aa62bccffbd1
Rich Tabor <hi@richtabor.com>
2018-12-12 09:19:54 -0500

    Create block-gallery-translations.php

---
+++ languages/block-gallery-translations.php
@@ -0,0 +1,448 @@
+<?php
+/* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
+$generated_i18n_strings = array(
+
+	// Reference: src/blocks/carousel/components/edit.js:197
+	/* translators: %s: Lowercase block title */
+	__( 'Drop to add to the %s', 'block-gallery' ),
+
+	// Reference: src/blocks/carousel/components/edit.js:233
+	// Reference: src/blocks/masonry/components/edit.js:177
+	// Reference: src/blocks/stacked/components/edit.js:150
+	__( '%s Gallery', 'block-gallery' ),
+
+	// Reference: src/blocks/carousel/components/edit.js:331
+	// Reference: src/components/gallery-image.js:193
+	__( 'Write caption…', 'block-gallery' ),
+
+	// Reference: src/blocks/carousel/components/inspector.js:66
+	// Reference: src/blocks/masonry/components/inspector.js:85
+	// Reference: src/blocks/stacked/components/inspector.js:86
+	__( '%s Settings', 'block-gallery' ),
+
+	// Reference: src/blocks/carousel/components/inspector.js:71
+	__( 'Size', 'block-gallery' ),
+
+	// Reference: src/blocks/carousel/components/inspector.js:84
+	// Reference: src/blocks/stacked/components/inspector.js:97
+	// Reference: src/components/responsive-tabs-control.js:36
+	__( 'Gutter', 'block-gallery' ),
+
+	// Reference: src/blocks/carousel/components/inspector.js:89
+	__( 'Height in pixels', 'block-gallery' ),
+
+	// Reference: src/blocks/carousel/components/inspector.js:97
+	// Reference: src/blocks/masonry/components/inspector.js:95
+	// Reference: src/blocks/stacked/components/inspector.js:101
+	// Reference: src/components/background/panel.js:162
+	__( 'Rounded Corners', 'block-gallery' ),
+
+	// Reference: src/blocks/carousel/index.js:29
+	__( 'Carousel', 'block-gallery' ),
+
+	// Reference: src/blocks/carousel/index.js:34
+	// Reference: src/blocks/masonry/index.js:34
+	// Reference: src/blocks/stacked/index.js:34
+	__( 'gallery', 'block-gallery' ),
+
+	// Reference: src/blocks/carousel/index.js:35
+	// Reference: src/blocks/masonry/index.js:35
+	// Reference: src/blocks/stacked/index.js:35
+	__( 'images', 'block-gallery' ),
+
+	// Reference: src/blocks/carousel/index.js:36
+	// Reference: src/blocks/masonry/index.js:36
+	// Reference: src/blocks/stacked/index.js:36
+	__( 'photos', 'block-gallery' ),
+
+	// Reference: src/blocks/carousel/index.js:89
+	__( 'Display multiple images in a beautiful carousel gallery.', 'block-gallery' ),
+
+	// Reference: src/blocks/masonry/components/edit.js:142
+	// Reference: src/blocks/stacked/components/edit.js:126
+	__( 'Drop to add to the %s gallery', 'block-gallery' ),
+
+	// Reference: src/blocks/masonry/components/inspector.js:105
+	// Reference: src/blocks/stacked/components/inspector.js:116
+	__( 'Captions', 'block-gallery' ),
+
+	// Reference: src/blocks/masonry/components/inspector.js:112
+	__( 'Caption Style', 'block-gallery' ),
+
+	// Reference: src/blocks/masonry/components/inspector.js:120
+	// Reference: src/blocks/stacked/components/inspector.js:130
+	__( 'Link Settings', 'block-gallery' ),
+
+	// Reference: src/blocks/masonry/components/inspector.js:124
+	// Reference: src/blocks/stacked/components/inspector.js:134
+	__( 'Link To', 'block-gallery' ),
+
+	// Reference: src/blocks/masonry/components/inspector.js:58
+	// Reference: src/blocks/stacked/components/inspector.js:55
+	__( 'Showing captions for each media item.', 'block-gallery' ),
+
+	// Reference: src/blocks/masonry/components/inspector.js:58
+	// Reference: src/blocks/stacked/components/inspector.js:55
+	__( 'Toggle to show media captions.', 'block-gallery' ),
+
+	// Reference: src/blocks/masonry/components/inspector.js:88
+	__( 'Column Size', 'block-gallery' ),
+
+	// Reference: src/blocks/masonry/components/inspector.js:96
+	__( 'Add rounded corners to the gallery items.', 'block-gallery' ),
+
+	// Reference: src/blocks/masonry/index.js:29
+	__( 'Masonry', 'block-gallery' ),
+
+	// Reference: src/blocks/masonry/index.js:53
+	__( 'Display multiple images in an organized masonry gallery.', 'block-gallery' ),
+
+	// Reference: src/blocks/stacked/components/inspector.js:111
+	__( 'Box Shadow', 'block-gallery' ),
+
+	// Reference: src/blocks/stacked/components/inspector.js:51
+	__( 'Fullwidth images are enabled.', 'block-gallery' ),
+
+	// Reference: src/blocks/stacked/components/inspector.js:51
+	__( 'Toggle to fill the available gallery area with completely fullwidth images.', 'block-gallery' ),
+
+	// Reference: src/blocks/stacked/components/inspector.js:89
+	__( 'Fullwidth Images', 'block-gallery' ),
+
+	// Reference: src/blocks/stacked/components/inspector.js:89
+	__( 'Fullwidth Image', 'block-gallery' ),
+
+	// Reference: src/blocks/stacked/index.js:29
+	__( 'Stacked', 'block-gallery' ),
+
+	// Reference: src/blocks/stacked/index.js:68
+	__( 'Display multiple images in an single column stacked gallery.', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:118
+	__( 'Top Left', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:119
+	__( 'Top Center', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:120
+	__( 'Top Right', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:121
+	__( 'Center Left', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:122
+	__( 'Center Center', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:123
+	__( 'Center Right', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:124
+	__( 'Bottom Left', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:125
+	__( 'Bottom Center', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:126
+	__( 'Bottom Right', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:130
+	__( 'No Repeat', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:132
+	__( 'Repeat Horizontally', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:133
+	__( 'Repeat Vertically', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:137
+	__( 'Auto', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:138
+	__( 'Cover', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:139
+	__( 'Contain', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:148
+	__( 'Background Settings', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:152
+	__( 'Padding', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:173
+	__( 'Image Overlay', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:182
+	__( 'Position', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:189
+	__( 'Repeat', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:196
+	__( 'Display', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:203
+	__( 'Fixed Background', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:213
+	__( 'Remove Background Image', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:220
+	__( 'Color Settings', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:74
+	__( 'Background Color', 'block-gallery' ),
+
+	// Reference: src/components/background/panel.js:82
+	__( 'Caption Text Color', 'block-gallery' ),
+
+	// Reference: src/components/background/toolbar.js:50
+	__( 'Add Background Image', 'block-gallery' ),
+
+	// Reference: src/components/background/toolbar.js:59
+	__( 'Remove background image', 'block-gallery' ),
+
+	// Reference: src/components/gallery-image.js:185
+	__( 'Remove Image', 'block-gallery' ),
+
+	// Reference: src/components/gallery-placeholder.js:45
+	__( 'Drag images, upload new ones or select files from your library.', 'block-gallery' ),
+
+	// Reference: src/components/gallery-upload.js:96
+	__( 'Upload an image', 'block-gallery' ),
+
+	// Reference: src/components/global/toolbar.js:100
+	// Reference: src/utils/filter-options.js:12
+	__( 'Saturation', 'block-gallery' ),
+
+	// Reference: src/components/global/toolbar.js:105
+	// Reference: src/components/lightbox/options.js:11
+	// Reference: src/utils/caption-options.js:10
+	// Reference: src/utils/filter-options.js:13
+	__( 'Dark', 'block-gallery' ),
+
+	// Reference: src/components/global/toolbar.js:110
+	// Reference: src/utils/filter-options.js:14
+	__( 'Vintage', 'block-gallery' ),
+
+	// Reference: src/components/global/toolbar.js:115
+	// Reference: src/utils/filter-options.js:15
+	__( 'Original', 'block-gallery' ),
+
+	// Reference: src/components/global/toolbar.js:78
+	__( 'Edit Gallery', 'block-gallery' ),
+
+	// Reference: src/components/global/toolbar.js:86
+	__( 'Apply Filter', 'block-gallery' ),
+
+	// Reference: src/components/global/toolbar.js:90
+	// Reference: src/utils/filter-options.js:10
+	__( 'Grayscale', 'block-gallery' ),
+
+	// Reference: src/components/global/toolbar.js:95
+	// Reference: src/utils/filter-options.js:11
+	__( 'Sepia', 'block-gallery' ),
+
+	// Reference: src/components/lightbox/control.js:45
+	__( 'Enable Lightbox', 'block-gallery' ),
+
+	// Reference: src/components/lightbox/control.js:50
+	__( 'Lightbox style', 'block-gallery' ),
+
+	// Reference: src/components/lightbox/options.js:10
+	// Reference: src/utils/caption-options.js:11
+	__( 'Light', 'block-gallery' ),
+
+	// Reference: src/components/responsive-tabs-control.js:71
+	/* translators: Control name */
+	__( 'Mobile %s', 'block-gallery' ),
+
+	// Reference: src/components/size-control.js:114
+	__( 'Small', 'block-gallery' ),
+
+	// Reference: src/components/size-control.js:118
+	__( 'Medium', 'block-gallery' ),
+
+	// Reference: src/components/size-control.js:174
+	__( 'Select Size', 'block-gallery' ),
+
+	// Reference: src/components/size-control.js:192
+	__( 'Reset', 'block-gallery' ),
+
+	// Reference: src/components/size-control.js:61
+	__( 'Large', 'block-gallery' ),
+
+	// Reference: src/components/size-control.js:65
+	__( 'Extra Large', 'block-gallery' ),
+
+	// Reference: src/components/slider-panel.js:28
+	__( 'seconds', 'block-gallery' ),
+
+	// Reference: src/components/slider-panel.js:28
+	__( 'second', 'block-gallery' ),
+
+	// Reference: src/components/slider-panel.js:31
+	/* translators: 1. Speed of the slider, 2: Time until the slide advances */
+	__( 'Automatically advancing to the next gallery item after %1$d %2$s.', 'block-gallery' ),
+
+	// Reference: src/components/slider-panel.js:31
+	/* translators: 1. Speed of the slider, 2: Time until the slide advances */
+	__( 'Automatically advance to the next gallery item after a set duration.', 'block-gallery' ),
+
+	// Reference: src/components/slider-panel.js:35
+	__( 'Dragging and flicking enabled on desktop and mobile devices.', 'block-gallery' ),
+
+	// Reference: src/components/slider-panel.js:35
+	__( 'Toggle to enable drag functionality on desktop and mobile devices.', 'block-gallery' ),
+
+	// Reference: src/components/slider-panel.js:39
+	__( 'Showing slide navigation arrows.', 'block-gallery' ),
+
+	// Reference: src/components/slider-panel.js:39
+	__( 'Toggle to show slide navigation arrows.', 'block-gallery' ),
+
+	// Reference: src/components/slider-panel.js:43
+	__( 'Showing dot navigation arrows.', 'block-gallery' ),
+
+	// Reference: src/components/slider-panel.js:43
+	__( 'Toggle to show dot navigation.', 'block-gallery' ),
+
+	// Reference: src/components/slider-panel.js:63
+	__( 'Slider Settings', 'block-gallery' ),
+
+	// Reference: src/components/slider-panel.js:65
+	__( 'Autoplay Slides', 'block-gallery' ),
+
+	// Reference: src/components/slider-panel.js:71
+	__( 'Transition Speed', 'block-gallery' ),
+
+	// Reference: src/components/slider-panel.js:79
+	__( 'Draggable', 'block-gallery' ),
+
+	// Reference: src/components/slider-panel.js:85
+	__( 'Arrow Navigation', 'block-gallery' ),
+
+	// Reference: src/components/slider-panel.js:91
+	__( 'Dot Navigation', 'block-gallery' ),
+
+	// Reference: src/utils/autoplay-options.js:10
+	__( 'One Second', 'block-gallery' ),
+
+	// Reference: src/utils/autoplay-options.js:11
+	__( 'Two Seconds', 'block-gallery' ),
+
+	// Reference: src/utils/autoplay-options.js:12
+	__( 'Three Seconds', 'block-gallery' ),
+
+	// Reference: src/utils/autoplay-options.js:13
+	__( 'Four Seconds', 'block-gallery' ),
+
+	// Reference: src/utils/autoplay-options.js:14
+	__( 'Five Seconds', 'block-gallery' ),
+
+	// Reference: src/utils/link-options.js:10
+	__( 'Attachment Page', 'block-gallery' ),
+
+	// Reference: src/utils/link-options.js:11
+	__( 'Media File', 'block-gallery' ),
+
+	// Reference: src/utils/link-options.js:12
+	__( 'None', 'block-gallery' ),
+
+	/* Plugin Name of the plugin */
+	__( 'Block Gallery', 'block-gallery' ),
+
+	/* Plugin URI of the plugin */
+	__( 'https://wpblockgallery.com/', 'block-gallery' ),
+
+	/* Description of the plugin */
+	__( 'Block Gallery is a suite of beautiful gallery blocks for the WordPress Gutenberg block editor..', 'block-gallery' ),
+
+	/* Author of the plugin */
+	__( 'Rich Tabor', 'block-gallery' ),
+
+	/* Author URI of the plugin */
+	__( 'https://richtabor.com/', 'block-gallery' ),
+
+	// Reference: class-block-gallery.php:84
+	// Reference: class-block-gallery.php:96
+	__( 'Cheating huh?', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-action-links.php:55
+	__( 'Go Pro', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-action-links.php:88
+	__( 'View Block Gallery Documentation', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-action-links.php:88
+	__( 'Help & FAQs', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-action-links.php:89
+	__( 'Review Block Gallery on WordPress.org', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-action-links.php:89
+	// Reference: includes/admin/class-block-gallery-feedback.php:285
+	__( 'Leave a Review', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-footer-text.php:47
+	/* translators: 1: Plugin Name, 2: Link to plugin review */
+	__( 'Enjoying %1$s? Please leave a %2$s rating — we appreciate your support!', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-feedback.php:95
+	/* translators: Number of years */
+	__( '%s years', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-feedback.php:97
+	__( 'a year', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-feedback.php:104
+	/* translators: Number of weeks */
+	__( '%s weeks', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-feedback.php:106
+	__( 'a week', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-feedback.php:113
+	/* translators: Number of days */
+	__( '%s days', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-feedback.php:115
+	__( 'a day', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-feedback.php:122
+	/* translators: Number of hours */
+	__( '%s hours', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-feedback.php:124
+	__( 'an hour', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-feedback.php:131
+	/* translators: Number of minutes */
+	__( '%s minutes', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-feedback.php:133
+	__( 'a minute', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-feedback.php:140
+	/* translators: Number of seconds */
+	__( '%s seconds', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-feedback.php:142
+	__( 'a second', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-feedback.php:274
+	/* translators: 1. Name */
+	__( '%1$s WordPress Plugin', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-feedback.php:278
+	/* translators: 1. Name */
+	__( 'Are you enjoying %1$s?', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-feedback.php:281
+	/* translators: 1. Name, 2. Time */
+	__( 'You have been using %1$s for %2$s now! Mind leaving a quick review to let us know know what you think? I\'d really appreciate it!', 'block-gallery' ),
+
+	// Reference: includes/admin/class-block-gallery-feedback.php:286
+	__( 'No thanks / I already have', 'block-gallery' ),
+);
+/* THIS IS THE END OF THE GENERATED FILE */
